e6872281f9d2a4a2f5ce4b24513008f0ac47c368
Rafael <rafaelcdmm@gmail.com>
2017-04-04 22:12:41 -0300

    Removendo ClienteRepositoryInterfaceTest
    Removendo ClienteRepositoryInterfaceTest

---
+++ src/test/java/bancuzim/interfaces/repository/AgenciaRepositoryTest.java
@@ -0,0 +1,30 @@
+package bancuzim.interfaces.repository;
+
+import bancuzim.repository.AgenciaRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class AgenciaRepositoryTest {
+
+   @Autowired
+   private AgenciaRepository agenciaRepository;
+
+    @Test
+    public void salvarTest() throws Exception {
+
+    }
+
+    @Test
+    public void buscarPorIdTest() throws Exception {
+
+    }
+
+
+}
