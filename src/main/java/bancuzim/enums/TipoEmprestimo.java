efbb041765082263899d485be56bc7290a39b96a
Rafael <rafaelcdmm@gmail.com>
2017-04-16 15:44:37 -0300

    Progredindo métodos do Enum TipoEmprestimo
    Progredindo métodos do Enum TipoEmprestimo

--- src/main/java/bancuzim/enums/TipoEmprestimo.java
+++ src/main/java/bancuzim/enums/TipoEmprestimo.java
@@ -1,5 +1,63 @@
 package bancuzim.enums;
 
 public enum TipoEmprestimo {
-    PESSOAL, IMOVEL, VEICULO
+    PESSOAL("PESSOAL"), IMOVEL("IMOVEL"), VEICULO("VEICULO");
+
+
+    public String tipoEmprestimo;
+
+    TipoEmprestimo(String tipoConta) {
+        this.tipoEmprestimo = tipoConta;
+    }
+
+    public String getTipoEmprestimo() {
+        return tipoEmprestimo;
+    }
+
+
+    public static boolean contains(String string) {
+
+        for (TipoEmprestimo tipoEmprestimo : TipoEmprestimo.values()) {
+            if (tipoEmprestimo.name().equals(string)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean notContains(String string) {
+
+        for (TipoEmprestimo tipoEmprestimo : TipoEmprestimo.values()) {
+            if (tipoEmprestimo.name().equals(string)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPESSOAL(TipoEmprestimo tipoConta){
+        return tipoConta.equals(TipoEmprestimo.PESSOAL);
+    }
+
+    public static boolean notPESSOAL(TipoEmprestimo tipoConta){
+        return !tipoConta.equals(TipoEmprestimo.PESSOAL);
+    }
+
+    public static boolean isIMOVEL(TipoEmprestimo tipoConta){
+        return tipoConta.equals(TipoEmprestimo.IMOVEL);
+    }
+
+    public static boolean notIMOVEL(TipoEmprestimo tipoConta){
+        return !tipoConta.equals(TipoEmprestimo.IMOVEL);
+    }
+
+    public static boolean isVEICULO(TipoEmprestimo tipoConta){
+        return tipoConta.equals(TipoEmprestimo.VEICULO);
+    }
+
+    public static boolean notVEICULO(TipoEmprestimo tipoConta){
+        return !tipoConta.equals(TipoEmprestimo.VEICULO);
+    }
 }
